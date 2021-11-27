db37cb6981f4dc2f75c999a160e7f1c259a55229
Martin Pan <wpan03@email.wm.edu>
2021-11-27 17:57:23 -0600

    a skeleton of the app

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Toxic Comment Classifier")
+
+text_input = st.text_input(label="Input Text")
+
+if st.button(label="Make and Explain Prediction"):
+    st.write("This function is still being developed.")
