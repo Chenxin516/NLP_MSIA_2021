15ee31807a23031c511d62400ef46051c211a602
Martin Pan <wpan03@email.wm.edu>
2021-11-29 20:36:25 -0600

    generate some data from scripts

--- run.py
+++ run.py
@@ -2,13 +2,14 @@ from pathlib import Path
 import pandas as pd
 import argparse
 
+from sklearn.model_selection import train_test_split
 
 from src.preprocess import convert_dataframe_to_bool, create_binary_label
+from src.fasttext_utility import df_to_txt
 
 data_path = Path("data")
 raw_path = data_path / "raw"
 interim_path = data_path / "interim"
-output_file_name = "train.parquet"
 
 if __name__ == "__main__":
 
@@ -18,14 +19,19 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "step",
-        default="preprocess",
+        default="preprocess-train",
         help="Which step to run",
-        choices=["preprocess"],
+        choices=[
+            "preprocess-train",
+            "preprocess-test",
+            "fasttext-prepare",
+        ],
     )
 
     args = parser.parse_args()
 
-    if args.step == "preprocess":
+    if args.step == "preprocess-train":
+        output_file_name = "train.parquet"
         try:
             interim_path.mkdir(parents=True, exist_ok=False)
         except:
@@ -36,3 +42,26 @@ if __name__ == "__main__":
         df_processed = convert_dataframe_to_bool(df_tmp)
         df_processed.to_parquet(interim_path / output_file_name)
         print(f"The file is written to {interim_path / output_file_name}")
+
+    elif args.step == "preprocess-test":
+        output_file_name = "test.parquet"
+        df_test = pd.read_csv(raw_path / "test_private_expanded.csv")
+        df_test = create_binary_label(df_test, target_col="toxicity")
+        df_test = convert_dataframe_to_bool(df_test)
+        df_test = df_test.head(10000).reset_index(drop=True)
+        df_test.to_parquet(interim_path / "test.parquet")
+        print(f"The file is written to {interim_path / output_file_name}")
+
+    elif args.step == "fasttext-prepare":
+        df = pd.read_parquet(interim_path / "train.parquet")
+        df["comment_text"] = df["comment_text"].str.lower()
+        df_train, df_valid = train_test_split(df, test_size=0.2, random_state=32)
+        for a_df, name in zip([df_train, df_valid], ["train", "valid"]):
+            output_file_name = interim_path / f"fasttext_{name}.txt"
+            df_to_txt(
+                a_df,
+                input_col="comment_text",
+                label_col="label",
+                output_file_path=output_file_name,
+            )
+            print(f"The file is written to {interim_path / output_file_name}")
